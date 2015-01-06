2de692c9b9a5bf0086a551237ebb849473da6a00
John Shahbazian <jshahbazi@hotmail.com>
2015-01-06 21:12:53 +0000

    split thetas

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,13 @@ int main () {
      
      mat combined_theta = join_rows(initial_theta1,initial_theta2);
      //vec initial_nn_params = vectorise(combined_theta);
+     std::cout << "combined_theta rows: " << combined_theta.n_rows << ", cols: " << combined_theta.n_cols << endl;
      
+     initial_theta1 = combined_theta.submat(0, 0, 0, initial_theta1.n_cols-1);
+     std::cout << "initial_theta1 rows: " << initial_theta1.n_rows << ", cols: " << initial_theta1.n_cols << endl;
+     
+     initial_theta2 = combined_theta.submat(0, initial_theta1.n_cols-1, 0,initial_theta1.n_cols-1+initial_theta2.n_cols-1);
+     std::cout << "initial_theta2rows: " << initial_theta2.n_rows << ", cols: " << initial_theta2.n_cols << endl;
      
      mat m3(training_size,hidden_layer_size,fill::zeros);
      
