0eb0f7b57ec2bf687aade12c6ef8671982ebecfc
John Shahbazian <jshahbazi@hotmail.com>
2015-01-06 20:11:45 +0000

    set constants, load data files, reshape theta commands

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,39 @@
 
 using namespace arma;
 
+constexpr int training_size = 4000;
+constexpr int input_layer_size = 784;
+constexpr int hidden_layer_size = 25;
+constexpr int num_labels = 10;
+
+constexpr double lambda = 0.1;
+constexpr int max_iterations = 100;
+
 int main () {
      std::cout <<  "Starting program..." << std::endl;
 
-     mat m1 = randu<mat>(40000,500);
-     mat m2 = randu<mat>(500,2000);
-     mat m3(40000,2000);
-     mat m4(40000,2000);
+     mat x_train(training_size,input_layer_size);
+     x_train.load("train_x_4000.csv");
+     
+     mat initial_theta1(input_layer_size,hidden_layer_size,fill::randu);
+     mat initial_theta2(hidden_layer_size,num_labels,fill::randu);
+     
+     initial_theta1.reshape(1,input_layer_size*hidden_layer_size);
+     initial_theta2.reshape(1,hidden_layer_size*num_labels);
+     
+     mat combined_theta = join_rows(initial_theta1,initial_theta2);
+     //vec initial_nn_params = vectorise(combined_theta);
+     
+     
+     mat m3(training_size,hidden_layer_size,fill::zeros);
+     
+     mat y_train(1,training_size);
+     y_train.load("train_y_4000.csv");
+     
 
-     m3=m1*m2;
+     //m3=x_train*initial_theta1;
 
-     std::cout << m3.row(0) << std::endl;
+     //std::cout << m3.row(0) << std::endl;
 
 
      return 0;
