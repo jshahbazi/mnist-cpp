aea4c95885c62d044416e323629934e9ac4ed116
jshahbazi <jshahbazi@gmail.com>
2015-01-06 22:30:11 -0500

    calculate mean and std deviation of training data

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,23 @@ int main () {
 
      mat x_train(training_size,input_layer_size);
      x_train.load("train_x_4000.csv");
-     
+
+     mat y_train(1,training_size);
+     y_train.load("train_y_4000.csv");
+
+     //calculate mean of the training data
+     double x_mean = sum(sum(x_train));
+     x_mean /= x_train.n_elem;  //33.5026  TODO: check this
+
+     //calculate standard devication of the training data
+     mat x_train_mean(training_size,input_layer_size);
+     x_train_mean = x_train;
+     x_train_mean.transform( [x_mean](double val){return (val - x_mean);} );
+     double x_std = sum(sum(x_train_mean));
+     x_std /= (x_train_mean.n_elem);
+     x_std = std::sqrt(x_std);  //7.23942e-06  TODO: check this
+
+
      mat initial_theta1(input_layer_size,hidden_layer_size,fill::randu);
      mat initial_theta2(hidden_layer_size,num_labels,fill::randu);
      
@@ -24,25 +40,22 @@ int main () {
      initial_theta2.reshape(1,hidden_layer_size*num_labels);
      
      mat combined_theta = join_rows(initial_theta1,initial_theta2);
-     //vec initial_nn_params = vectorise(combined_theta);
      std::cout << "combined_theta rows: " << combined_theta.n_rows << ", cols: " << combined_theta.n_cols << endl;
-     
+
+
+     //call fmincg function
+
+
      initial_theta1 = combined_theta.submat(0, 0, 0, initial_theta1.n_cols-1);
+     initial_theta1.reshape(input_layer_size, hidden_layer_size);
      std::cout << "initial_theta1 rows: " << initial_theta1.n_rows << ", cols: " << initial_theta1.n_cols << endl;
-     
+
      initial_theta2 = combined_theta.submat(0, initial_theta1.n_cols-1, 0,initial_theta1.n_cols-1+initial_theta2.n_cols-1);
-     std::cout << "initial_theta2rows: " << initial_theta2.n_rows << ", cols: " << initial_theta2.n_cols << endl;
-     
-     mat m3(training_size,hidden_layer_size,fill::zeros);
-     
-     mat y_train(1,training_size);
-     y_train.load("train_y_4000.csv");
-     
-
-     //m3=x_train*initial_theta1;
-
-     //std::cout << m3.row(0) << std::endl;
+     initial_theta2.reshape(hidden_layer_size, num_labels);
+     std::cout << "initial_theta2 rows: " << initial_theta2.n_rows << ", cols: " << initial_theta2.n_cols << endl;
 
 
      return 0;
 }
+
+
